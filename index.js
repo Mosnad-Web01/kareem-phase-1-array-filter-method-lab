cad79890b6a8bdd7e3e06b927f0108ac8b74b49e
kareemAlqubati <salamkareem792@gmail.com>
2024-08-25 11:12:14 +0300

    Compelet

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase().includes(string.toLowerCase());
+  })
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase().startsWith(string.toLowerCase());
+  })
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function (driver) {
+    return driver.name.toLowerCase().includes(string.toLowerCase());
+  })
+}
